a44a92f318a812dd2baa46b5d61cb7da2d12aea6
dungh <dunghasd@gmail.com>
2024-09-05 19:31:29 +0900

    테스트코드 환경설정 구성 진행 ..

--- src/test/java/com/cmj/security/SecurityApplicationTests.java
+++ src/test/java/com/cmj/security/SecurityApplicationTests.java
@@ -1,9 +1,13 @@
 package com.cmj.security;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@DataJpaTest
+@TestPropertySource(locations = "classpath:application-test.yml")
 class SecurityApplicationTests {
 
     @Test

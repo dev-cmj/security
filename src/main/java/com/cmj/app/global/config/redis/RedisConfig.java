2f61df85ed195eede746b43b836aa015286d0854
dungh <dunghasd@gmail.com>
2024-09-17 20:58:07 +0900

    Redis 방법 구상..

--- src/main/java/com/cmj/app/global/config/redis/RedisConfig.java
+++ src/main/java/com/cmj/app/global/config/redis/RedisConfig.java
@@ -59,4 +59,10 @@ public class RedisConfig {
     public CacheErrorHandler errorHandler() {
         return new CustomCacheErrorHandler();
     }
+
+// Redis에 JWT 토큰을 저장할 경우, 토큰의 만료 시간을 설정하고, 토큰이 만료되었을 때 자동으로 삭제되도록 설정하는 것이 좋다.
+// 이렇게 하면 Redis에 만료된 토큰이 남아있지 않아 불필요한 메모리 사용을 방지할 수 있다.
+// 단, 로그아웃 기능이 구현된 경우, 로그아웃 시 Refresh Token을 Black List에 저장하여 해당 토큰이 이후에 재사용되지 않도록 관리할 수 있다.
+// Black List에 저장된 토큰은 만료 시간과 관계없이 유효하지 않은 상태로 간주되며, 로그아웃 시 명시적으로 처리해야 한다.
+// 이를 통해 사용자가 로그아웃한 후에도 해당 토큰을 이용한 인증 시도가 무효화되도록 한다.
 }

98248a959484ff5dfae125130d26fe0f04db7ca2
mjchoi <dunghasd@gmail.com>
2024-09-29 20:17:27 +0900

    스케줄러 구현 진행(saveViewCountFromRedisToDB) 고도화

--- src/main/java/com/cmj/app/domain/post/repository/PostRepository.java
+++ src/main/java/com/cmj/app/domain/post/repository/PostRepository.java
@@ -4,11 +4,16 @@ import com.cmj.app.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     @Modifying
     @Query("update Post p set p.viewCount = p.viewCount + 1 where p.id = :postId")
-    void increaseViewCount(Long postId);
+    void increaseViewCount(@Param("postId") Long postId);
+
+    @Modifying
+    @Query("UPDATE Post p SET p.viewCount = :viewCount WHERE p.id = :postId")
+    void updateViewCount(@Param("postId") Long postId, @Param("viewCount") Long viewCount);
 }
--- src/main/java/com/cmj/app/domain/post/service/ViewCountService.java
+++ src/main/java/com/cmj/app/domain/post/service/ViewCountService.java
@@ -11,7 +11,9 @@ import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -76,18 +78,24 @@ public class ViewCountService {
     }
 
     //Redis에 있는 조회수를 DB에 저장하는 스케줄러 로직, 같거나 더 많은 조회수가 있을 경우 DB에 저장
+    @Transactional
     public void saveViewCountFromRedisToDB() {
-        redisTemplate.keys("post:viewcount:*")
-                .forEach(key -> {
-                    String postId = key.split(":")[2];
-                    Long redisViewCount = Long.parseLong(redisTemplate.opsForValue().get(key));
-                    Long dbViewCount = postRepository.findById(Long.parseLong(postId))
-                            .map(Post::getViewCount)
-                            .orElse(0L);
+        List<Post> postsToUpdate = new ArrayList<>();
 
-                    if (redisViewCount >= dbViewCount) {
-                        postRepository.increaseViewCount(Long.parseLong(postId));
-                    }
+        redisTemplate.keys("post:viewcount:*").forEach(key -> {
+            String postId = key.split(":")[2];
+            Long redisViewCount = Long.parseLong(redisTemplate.opsForValue().get(key));
+            Optional<Post> post = postRepository.findById(Long.parseLong(postId));
+            Long dbViewCount = post.map(Post::getViewCount).orElse(0L);
+
+            if (redisViewCount >= dbViewCount) {
+                post.ifPresent(p -> {
+                    p.setViewCount(redisViewCount);  // 엔티티 필드 수정
+                    postsToUpdate.add(p);  // 업데이트할 엔티티 목록에 추가
                 });
+            }
+        });
+
+        postRepository.saveAll(postsToUpdate);  // 배치 저장
     }
 }
